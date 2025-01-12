b45a67aadfe996cca9e4260c64d67ac01176b69d
ainadlina <ainadlina14@gmail.com>
2025-01-12 10:05:19 +0800

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -37,6 +37,16 @@ export default function Footer(){
                         <li className="hover:text-primary text-lg leading-10 cursor-pointer">Contact us</li>
                     </ul>
                 </div>
+                <div>
+                    <h5 className="font-semibold text-2xl pb-8">Want to know more?</h5>
+                    <p className="text-gray py-4 text-lg pb-4">Subscribe to our website and get exciting offers</p>
+                    <div className="h-14 flex items-center relative">
+                        <input type="text" placeholder="Enter your email address" className="bg-white outline-none w-full h-full shadow px-4"/>
+                        <button className="bg-primary text-white rounded-full absolute right-2 w-10 h-10">
+                            <FaArrowAltCircleUp className="w-full h-full" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
